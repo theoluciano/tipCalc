b0022a742b1ae261ebd8c7f3a0797f54d0bac0f4
Theo Luciano <62947477+theoluciano@users.noreply.github.com>
2021-11-16 09:55:46 -0500

    WIP: calculating tip; updating output content

--- tipCalc.js
+++ tipCalc.js
@@ -1,4 +1,4 @@
-function getBillInput(){
+const totalBeforeTip = () => {
   var billInput;
   billInput = document.getElementById("bill-input").value;
   twoPlaceTotal = parseFloat(billInput);
@@ -6,43 +6,67 @@ function getBillInput(){
   return twoPlaceTotal;
 }
 
-function getPeopleInput(){
+const numberOfPeople = () => {
   var peopleInput = document.getElementById("people-input").value;
   peopleTotal = parseInt(peopleInput);
   // console.log(peopleTotal);
   return peopleTotal;
 }
 
-function clickReset(){
+const clickReset = () => {
   var billInput = document.getElementById("bill-input");
   var peopleInput = document.getElementById("people-input");
+  var radioValue = document.getElementById("custom");
   billInput.value = "";
   peopleInput.value = "";
+  radioValue.value = "";
+  document.getElementById("tip-total").textContent = "$0.00";
+  document.getElementById("per-person-total").textContent = "$0.00";
+
   //document.getElementById("tip-total").textContent="$45.00"
 }
  
-function clearRadio(){
-    var customInputValue = document.getElementById("custom").value;
-    if (customInputValue.active = true){
-      var radioValue = document.getElementsByClassName("radio-button");
-      for(i=0;i<radioValue.length;i++){
-        radioValue[i].checked = false;
-      }
-    }else{
-      // return customInputValue; need to figure out how to return the value of the custom input
-      console.log(customInputValue.value);
-    }
+const clearCustomInput = () => {
+  var radioValue = document.getElementsByClassName("radio-button");
+  for(i=0;i<radioValue.length;i++){
+    radioValue[i].checked = false;
   }
+  
+}
 
-  function clearCustomInput(){
-    const radiovalue = document.getElementsByClassName("radio-button");
-    if (radiovalue.active = true){
-      var customInputValue = document.getElementById("custom");
-      customInputValue.value = "";
-    }else{
-      // return radiovalue;
-      console.log(radiovalue)
-    }
-  }
+const customTipPercentage = () =>{
+  var inputValue = document.getElementById("custom").value
+  var inputOutput = parseFloat("0." + inputValue).toFixed(2);
+  return inputOutput;
+  // console.log(inputOutput);
+
+}
+
+const tipPercentage = () => {
+  var radioValue = document.querySelector('input[name="tip-percent"]:checked').value;
+  var customInputValue = document.getElementById("custom");
+  customInputValue.value = "";
+  return radioValue;
+  // console.log(radioValue);
+}
+
+const postTip = () =>{
+  const tipPercentage = window.tipPercentage();
+  const customTipPercentage = window.customTipPercentage();
+  const numberOfPeople = window.numberOfPeople();
+  const totalBeforeTip = window.totalBeforeTip();
+
+  var totalOutput = (totalBeforeTip * tipPercentage) / numberOfPeople;
+  var totalPerPerson = (totalBeforeTip + (totalBeforeTip * tipPercentage)) / numberOfPeople;
+  document.getElementById("tip-total").textContent = "$" + parseFloat(totalOutput).toFixed(2);
+  document.getElementById("per-person-total").textContent = "$" + parseFloat(totalPerPerson).toFixed(2);
+}
+
+window.totalBeforeTip = totalBeforeTip
+window.numberOfPeople = numberOfPeople
+window.clickReset = clickReset
+window.clearCustomInput = clearCustomInput
+window.customTipPercentage = customTipPercentage
+window.tipPercentage = tipPercentage
 
 
